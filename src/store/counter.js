0f67421118030b31b8248b3f980892c56f8f7f65
IlyaAgarishev <ilyagarishev@gmail.com>
2022-08-07 23:29:50 +0500

    mobx counter init

---
+++ src/store/counter.js
@@ -0,0 +1,19 @@
+import { makeAutoObservable } from "mobx";
+
+class Counter {
+  count = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increment() {
+    this.count = this.count + 1;
+  }
+
+  decrement() {
+    this.count = this.count - 1;
+  }
+}
+
+export default new Counter();
